29587b6a27beee9bc7adadb4034d50580728944c
puddinging2209 <jinjin2008jp@gmail.com>
2025-12-26 00:52:09 +0900

    カメテテ

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import ReactModal from 'react-modal';
 import DepartureSection from './DepartureSection';
 import './App.css';
-import ReactModal from 'react-modal';
 
 function App() {
 
