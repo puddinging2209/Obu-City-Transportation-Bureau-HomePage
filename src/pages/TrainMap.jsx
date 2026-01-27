7ce816d9725fa1b441e80ef782050bc8db0231fd
ramune314159265 <ramune314159265@gmail.com>
2026-01-28 01:06:40 +0900

    ガバイト

--- src/pages/TrainMap.jsx
+++ src/pages/TrainMap.jsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import "leaflet/dist/leaflet.css";
 import React from 'react';
+import stations from '../data/stations.json';
+import trainMapData from '../data/trainMap.json';
 
 function TrainMap() {
 	const mapElementRef = React.useRef(null);
@@ -19,11 +21,28 @@ function TrainMap() {
 			const map = L.map(mapElement);
 			mapRef.current = map;
 
-			map.setView([35.681236, 139.767125], 15);
+			map.setView([35.0086145360, 136.9621485834], 12);
 			L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 				attribution:
 					'&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
 			}).addTo(map);
+			Object.values(trainMapData).forEach(d => {
+				L.polyline(d.line_coordinates.filter(l => l[0]).map(l => l.map(Number).toReversed()), {
+					color: d.color,
+					weight: 4,
+					opacity: 0.75
+				}).addTo(map);
+			})
+			Object.values(stations).forEach(s => {
+				L.marker([s.lat, s.lng], {
+					icon: L.divIcon({
+						html: `${s.name}`,
+						className: 'stationLabel',
+						iconSize: [120, 24],
+						iconAnchor: [6, 12],
+					})
+				}).addTo(map)
+			})
 		})()
 	}, [])
 
