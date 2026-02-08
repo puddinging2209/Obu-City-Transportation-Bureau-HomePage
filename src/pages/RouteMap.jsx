5af62fc41a2932c18ce737e2fc0e36919a477fb2
puddinging2209 <jinjin2008jp@gmail.com>
2026-02-08 15:49:01 +0900

    ユキカブリ

--- src/pages/RouteMap.jsx
+++ src/pages/RouteMap.jsx
@@ -13,7 +13,7 @@ function RouteMap() {
             <Typography variant="h6">路線図</Typography>
             <Stack sx={{ width: { xs: '100%', md: '70%' }, overflowX: 'auto', mx: 'auto' }}>
                 <img
-                    src={'../../public/routeMap/current.png'}
+                    src={'../routeMap/current.png'}
                     alt="路線図"
                     style={{ width: '100%', marginTop: '16px', borderRadius: '14px', cursor: 'zoom-in' }}
                     onClick={() => setIsOpenLightbox(true)}
