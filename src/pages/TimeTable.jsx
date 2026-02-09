f845fdb7e1f7f4ba70680d96ae3d8795b6944d52
puddinging2209 <jinjin2008jp@gmail.com>
2026-02-09 16:00:33 +0900

    ハッサム

--- src/pages/TimeTable.jsx
+++ src/pages/TimeTable.jsx
@@ -1,18 +1,31 @@
+import { Stack, Typography } from '@mui/material';
 import '@offlegacy/nuqs-hash-router';
 import { useQueryState } from 'nuqs';
 import StationSelecter from '../components/StationSelecter.jsx';
+
+import stations from '../data/stations.json';
 
 function TimeTable() {
 
     const [station, setStation] = useQueryState('station')
 
     return (
-        <StationSelecter
-            value={station}
-            placeholder={'駅を選択'}
-            onChange={(value => setStation(value.value))}
-            busStop={false}
-        />
+        <>
+            <Typography variant="h6">時刻表</Typography>
+            <Stack sx={{ mt: 2 }}>
+                <StationSelecter
+                    value={station ? {
+                        value: station,
+                        label: station,
+                        role: 'station',
+                        kana: stations[station]?.kana
+                    } : null}
+                    placeholder={'駅を選択'}
+                    onChange={(value => setStation(value.value))}
+                    busStop={false}
+                />
+            </Stack>
+        </>
     )
 }
 
