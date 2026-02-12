b7d639714a16c6a02a05f3fc3ec0b728e117ea82
puddinging2209 <jinjin2008jp@gmail.com>
2026-02-12 09:47:28 +0900

    ボクレー

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { createTheme } from '@mui/material';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 const Layout = React.lazy(() => import('./components/Layout.jsx'));
@@ -12,31 +11,7 @@ const Position = React.lazy(() => import('./pages/Position.jsx'));
 const RouteMap = React.lazy(() => import('./pages/RouteMap.jsx'));
 const TimeTable = React.lazy(() => import('./pages/TimeTable.jsx'));
 const Transfer = React.lazy(() => import('./pages/Transfer.jsx'));
-
-
-import About from './pages/About.jsx';
-import Home from './pages/Home.jsx';
-import Log from './pages/Log.jsx';
-import NewsList from './pages/Notice.jsx';
-import Position from './pages/Position.jsx';
-import RouteMap from './pages/RouteMap.jsx';
-import TimeTable from './pages/TimeTable.jsx';
-import Transfer from './pages/Transfer.jsx';
-
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#1976d2', // 地下鉄っぽい青
-    },
-    secondary: {
-      main: '#ff6600', // バス色
-    },
-  },
-  shape: {
-    borderRadius: 8,
-  },
-});
+const TrainMap = React.lazy(() => import('./pages/TrainMap.jsx'));
 
 function App() {
   return (
@@ -49,6 +24,7 @@ function App() {
           <Route path='/transfer' element={<Transfer />}></Route>
           <Route path='/timetable' element={<TimeTable />}></Route>
           <Route path='/position' element={<Position />}></Route>
+          <Route path='/trainmap' element={<TrainMap />}></Route>
           <Route path='/log' element={<Log />}></Route>
           <Route path='/news' element={<NewsList />}></Route>
           <Route path='/about' element={<About />}></Route>
