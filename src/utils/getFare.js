35f084fae4ef9df761b70ea4c01a8d96c2aa5c2f
puddinging2209 <jinjin2008jp@gmail.com>
2026-04-05 12:10:10 +0900

    ヨーギラス

--- src/utils/getFare.js
+++ src/utils/getFare.js
@@ -8,15 +8,10 @@ const longFare = 15;
 export default function getFare(distance) {
     let result = { regular: 170, ic: 170 };
     let fare = 0;
-    if (distance <= boundaries[0]) {
-        fare = firstFare;
-    } else if (distance <= boundaries[1]) {
-        fare = firstFare + (distance - boundaries[0]) * shortFare;
-    } else if (distance <= boundaries[2]) {
-        fare = firstFare + (distance - boundaries[0]) * middleFare;
-    } else {
-        fare = (distance - boundaries[0]) * longFare;
-    }
+    fare = firstFare +
+        Math.min(Math.max(0, distance - boundaries[0]), boundaries[1] - boundaries[0]) * shortFare +
+        Math.min(Math.max(0, distance - boundaries[1]), boundaries[2] - boundaries[1]) * middleFare +
+        Math.max(0, distance - boundaries[2]) * longFare;
     while (result.regular < fare) {
         result.regular += 30;
     }
