1e7e8d58bd0154fdffb2c7c8a5f2edb64ad37c8f
puddinging2209 <jinjin2008jp@gmail.com>
2026-05-12 22:22:30 +0900

    ジジーロン

--- src/pages/RouteMap.jsx
+++ src/pages/RouteMap.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import Lightbox from "yet-another-react-lightbox";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/styles.css";
@@ -22,12 +22,9 @@ function RouteMap() {
         <div style={{ alignItems: 'center' }}>
             <Typography variant="h6">路線図</Typography>
             <Stack sx={{ width: { xs: '100%', md: '70%' }, overflowX: 'auto', mx: 'auto' }}>
-                <img
-                    src={import.meta.env.BASE_URL + 'routeMap/202605.png'}
-                    alt="路線図"
-                    style={{ width: '100%', marginTop: '16px', borderRadius: '14px', cursor: 'zoom-in' }}
-                    onClick={() => setIsOpenLightbox(true)}
-                />
+                <Button variant="outlined" size="large" onClick={() => setIsOpenLightbox(true)} sx={{ mt: 2 }}>
+                    全線路線図を表示
+                </Button>
             </Stack>
             <Lightbox
                 open={isOpenLightbox}
