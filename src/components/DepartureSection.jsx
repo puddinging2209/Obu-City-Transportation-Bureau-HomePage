d321ee5854d6cf5104533030c7ed53ccecaa330c
puddinging2209 <jinjin2008jp@gmail.com>
2026-07-12 19:35:37 +0900

    駅ログに軽微な変更

--- src/components/DepartureSection.jsx
+++ src/components/DepartureSection.jsx
@@ -5,19 +5,19 @@ import { useNavigate } from 'react-router-dom';
 
 import AddIcon from '@mui/icons-material/Add';
 import {
-    Box,
-    Button,
-    Card,
-    CardActionArea,
-    Container,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    Stack,
-    Tab,
-    Tabs,
-    Typography,
+	Box,
+	Button,
+	Card,
+	CardActionArea,
+	Container,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	Stack,
+	Tab,
+	Tabs,
+	Typography,
 } from '@mui/material';
 
 import { addMyStationAtom, myStationsAtom, nearestStationAtom } from '../utils/Atom.js';
@@ -27,242 +27,242 @@ import DepartureCard from './DepartureCard.jsx';
 import StationSelecter from './StationSelecter.jsx';
 
 export default function DepartureSection() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const [content, setContent] = React.useState('nearest');
-    const [serchedStation, setSearchedStation] = React.useState(null);
+	const [content, setContent] = React.useState('nearest');
+	const [serchedStation, setSearchedStation] = React.useState(null);
 
-    const myStations = useAtomValue(myStationsAtom);
-    const addMyStation = useSetAtom(addMyStationAtom);
+	const myStations = useAtomValue(myStationsAtom);
+	const addMyStation = useSetAtom(addMyStationAtom);
 
-    const [nearestStation, setNearestStation] = React.useState(null);
+	const [nearestStation, setNearestStation] = React.useState(null);
 
-    const [nearestAtom, setNearestAtom] = useAtom(nearestStationAtom);
-    const [loadingNearest, setLoadingNearest] = React.useState(false);
+	const [nearestAtom, setNearestAtom] = useAtom(nearestStationAtom);
+	const [loadingNearest, setLoadingNearest] = React.useState(false);
 
-    function updateNearest() {
-        setLoadingNearest(true);
-        searchNearestStation()
-            .then((id) => {
-                setLoadingNearest(false);
-                setNearestStation(id);
-                setNearestAtom(id);
+	const updateVisited = (visited) => {
+		const updated = [];
+		for (const i in visited) {
+			const v = visited[i];
+			if (visited[i - 1]?.id === v.id) continue; // 同駅連続では更新しない
+			updated.push(v);
+		}
+		return updated;
+	};
 
-                const visited =
-                    localStorage.getItem('visitedStations') ?
-                        JSON.parse(localStorage.getItem('visitedStations'))
-                    :   [];
-                if (Date.now() - visited.toReversed().find((s) => s.id === id)?.time < 300000)
-                    return; // 5分以内は更新しない
-                if (visited[0]?.id) {
-                    localStorage.setItem(
-                        'visitedStations',
-                        JSON.stringify([...visited, { id, time: Date.now() }]),
-                    );
-                } else {
-                    localStorage.setItem(
-                        'visitedStations',
-                        JSON.stringify([{ id, time: Date.now() }]),
-                    );
-                }
-            })
-            .catch(() => {
-                setLoadingNearest(false);
-                alert('位置情報の取得に失敗しました');
-            });
-    }
+	const updateNearest = () => {
+		setLoadingNearest(true);
+		searchNearestStation()
+			.then((id) => {
+				setLoadingNearest(false);
+				setNearestStation(id);
+				setNearestAtom(id);
 
-    React.useEffect(() => {
-        if (!nearestAtom) {
-            updateNearest();
-        } else {
-            setNearestStation(nearestAtom);
-        }
-    }, []);
+				const visited = localStorage.getItem('visitedStations') ? JSON.parse(localStorage.getItem('visitedStations')) : [];
+				if (visited.at(-1)?.id === id) return; // 同駅連続では更新しない
+				localStorage.setItem('visitedStations', JSON.stringify([...updateVisited(visited), { id, time: Date.now() }]));
+			})
+			.catch(() => {
+				setLoadingNearest(false);
+				alert('位置情報の取得に失敗しました');
+			});
+	};
 
-    const [isShowSearch, setIsShowSearch] = React.useState(false);
+	React.useEffect(() => {
+		if (!nearestAtom) {
+			updateNearest();
+		} else {
+			setNearestStation(nearestAtom);
+		}
+	}, []);
 
-    return (
-        <Box>
-            <Container
-                sx={{
-                    mx: 'auto',
-                    pb: 2,
-                    width: { xs: '100%', md: 'fit-content' },
-                    textAlign: 'center',
-                }}
-            >
-                <Stack>
-                    <Tabs
-                        value={content}
-                        onChange={(event, newValue) => setContent(newValue)}
-                        centered
-                    >
-                        <Tab label='最寄り駅' value='nearest' />
-                        <Tab label='駅を検索' value='search' />
-                    </Tabs>
-                    {content === 'nearest' && (
-                        <Stack>
-                            <Stack
-                                sx={{
-                                    display: 'flex',
-                                    pb: 2,
-                                    width: { xs: '100%', md: '100%' },
-                                    justifyContent: 'space-between',
-                                    alignItems: 'flex-end',
-                                }}
-                            >
-                                <Button
-                                    sx={{
-                                        height: 36,
-                                        ml: 'auto',
-                                    }}
-                                    loading={loadingNearest}
-                                    onClick={() => {
-                                        if (!loadingNearest) updateNearest();
-                                    }}
-                                >
-                                    更新
-                                </Button>
-                            </Stack>
-                            {nearestStation ?
-                                <div width='100%'>
-                                    <DepartureCard
-                                        key={`near-${nearestStation}`}
-                                        station={{ id: nearestStation, role: 'station' }}
-                                        addButton
-                                    />
-                                </div>
-                            :   <Card
-                                    sx={{
-                                        width: { xs: '100%', md: 300 },
-                                        minHeight: 240,
-                                        position: 'relative',
-                                        flexShrink: 0,
-                                    }}
-                                >
-                                    <Typography variant='body2' sx={{ mt: 2 }}>
-                                        位置情報が取得できませんでした
-                                    </Typography>
-                                </Card>
-                            }
-                        </Stack>
-                    )}
-                    {content === 'search' && (
-                        <>
-                            <Stack
-                                sx={{
-                                    display: 'flex',
-                                    mt: 2,
-                                }}
-                            >
-                                <StationSelecter
-                                    onChange={(option) => {
-                                        if (option) {
-                                            setSearchedStation(option);
-                                        }
-                                    }}
-                                    value={serchedStation ? serchedStation : null}
-                                    autoFocus
-                                />
-                            </Stack>
-                            {serchedStation && (
-                                <Box sx={{ mt: 2, width: { xs: '100%', md: 300 } }}>
-                                    <DepartureCard
-                                        key={`search-${serchedStation.value}`}
-                                        station={{
-                                            id: serchedStation.value,
-                                            role: serchedStation.role,
-                                        }}
-                                        addButton
-                                    />
-                                </Box>
-                            )}
-                        </>
-                    )}
-                </Stack>
-            </Container>
+	const [isShowSearch, setIsShowSearch] = React.useState(false);
 
-            <Typography variant='h6' sx={{ mb: 2, textAlign: 'left' }}>
-                マイ駅・停留所
-            </Typography>
+	return (
+		<Box>
+			<Container
+				sx={{
+					mx: 'auto',
+					pb: 2,
+					width: { xs: '100%', md: 'fit-content' },
+					textAlign: 'center',
+				}}
+			>
+				<Stack>
+					<Tabs value={content} onChange={(event, newValue) => setContent(newValue)} centered>
+						<Tab label='最寄り駅' value='nearest' />
+						<Tab label='駅を検索' value='search' />
+					</Tabs>
+					{content === 'nearest' && (
+						<Stack>
+							<Stack
+								sx={{
+									display: 'flex',
+									pb: 2,
+									width: { xs: '100%', md: '100%' },
+									justifyContent: 'space-between',
+									alignItems: 'flex-end',
+								}}
+							>
+								<Button
+									sx={{
+										height: 36,
+										ml: 'auto',
+									}}
+									loading={loadingNearest}
+									onClick={() => {
+										if (!loadingNearest) updateNearest();
+									}}
+								>
+									更新
+								</Button>
+							</Stack>
+							{nearestStation ?
+								<div width='100%'>
+									<DepartureCard key={`near-${nearestStation}`} station={{ id: nearestStation, role: 'station' }} addButton />
+								</div>
+							:	<Card
+									sx={{
+										width: { xs: '100%', md: 300 },
+										minHeight: 240,
+										position: 'relative',
+										flexShrink: 0,
+									}}
+								>
+									<Typography variant='body2' sx={{ mt: 2 }}>
+										位置情報が取得できませんでした
+									</Typography>
+								</Card>
+							}
+						</Stack>
+					)}
+					{content === 'search' && (
+						<>
+							<Stack
+								sx={{
+									display: 'flex',
+									mt: 2,
+								}}
+							>
+								<StationSelecter
+									onChange={(option) => {
+										if (option) {
+											setSearchedStation(option);
+										}
+									}}
+									value={serchedStation ? serchedStation : null}
+									autoFocus
+								/>
+							</Stack>
+							<Box sx={{ mt: 2, width: { xs: '100%', md: 300 } }}>
+								{serchedStation ?
+									<DepartureCard
+										key={`search-${serchedStation.value}`}
+										station={{
+											id: serchedStation.value,
+											role: serchedStation.role,
+										}}
+										addButton
+									/>
+								:	<Card
+										sx={{
+											width: { xs: '100%', md: 300 },
+											minHeight: 240,
+											position: 'relative',
+											flexShrink: 0,
+										}}
+									>
+										<Typography variant='body2' sx={{ mt: 2 }}>
+											駅・停留所を検索してください
+										</Typography>
+									</Card>
+								}
+							</Box>
+						</>
+					)}
+				</Stack>
+			</Container>
 
-            <Stack
-                direction='row'
-                spacing={2}
-                sx={{
-                    overflowX: 'auto',
-                    whiteSpace: 'nowrap',
-                    flexWrap: 'nowrap',
-                    pb: 1,
-                    scrollSnapType: { xs: 'x mandatory', md: 'none' },
-                }}
-            >
-                {myStations.map((sta) => (
-                    <Box sx={{ scrollSnapAlign: { xs: 'center', md: 'none' } }} key={sta.id}>
-                        <Box sx={{ width: { xs: '85vw', md: 300 } }}>
-                            <DepartureCard key={`my-${sta.id}`} station={sta} removeButton />
-                        </Box>
-                    </Box>
-                ))}
-                <Card
-                    sx={{
-                        width: { xs: '85%', md: 300 },
-                        flexShrink: 0,
-                        scrollSnapAlign: { xs: 'center', md: 'none' },
-                    }}
-                    variant='outlined'
-                >
-                    <CardActionArea
-                        onClick={() => {
-                            setIsShowSearch(true);
-                            navigate('?modal=addStation');
-                        }}
-                        style={{ width: '100%', height: '100%' }}
-                    >
-                        <AddIcon fontSize='large' />
-                        <Typography align='center'>マイ駅を追加</Typography>
-                    </CardActionArea>
-                </Card>
-            </Stack>
+			<Typography variant='h6' sx={{ mb: 2, textAlign: 'left' }}>
+				マイ駅・停留所
+			</Typography>
 
-            <Dialog
-                open={isShowSearch}
-                onClose={() => {
-                    setIsShowSearch(false);
-                    navigate('/home');
-                }}
-                fullWidth
-            >
-                <DialogTitle>
-                    <Typography variant='h6' component='div'>
-                        マイ駅・停留所を追加
-                    </Typography>
-                </DialogTitle>
-                <DialogContent>
-                    <StationSelecter
-                        onChange={(selected) => {
-                            if (selected) {
-                                addMyStation({ id: selected.value, role: selected.role });
-                                setIsShowSearch(false);
-                                navigate('/home');
-                            }
-                        }}
-                        autoFocus
-                        disabledStations={myStations.map((s) => s.name)}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={() => {
-                            navigate('/home');
-                            setIsShowSearch(false);
-                        }}
-                    >
-                        閉じる
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </Box>
-    );
+			<Stack
+				direction='row'
+				spacing={2}
+				sx={{
+					overflowX: 'auto',
+					whiteSpace: 'nowrap',
+					flexWrap: 'nowrap',
+					pb: 1,
+					scrollSnapType: { xs: 'x mandatory', md: 'none' },
+				}}
+			>
+				{myStations.map((sta) => (
+					<Box sx={{ scrollSnapAlign: { xs: 'center', md: 'none' } }} key={sta.id}>
+						<Box sx={{ width: { xs: '85vw', md: 300 } }}>
+							<DepartureCard key={`my-${sta.id}`} station={sta} removeButton />
+						</Box>
+					</Box>
+				))}
+				<Card
+					sx={{
+						width: { xs: '85%', md: 300 },
+						flexShrink: 0,
+						scrollSnapAlign: { xs: 'center', md: 'none' },
+					}}
+					variant='outlined'
+				>
+					<CardActionArea
+						onClick={() => {
+							setIsShowSearch(true);
+							navigate('?modal=addStation');
+						}}
+						style={{ width: '100%', height: '100%' }}
+					>
+						<AddIcon fontSize='large' />
+						<Typography align='center'>マイ駅を追加</Typography>
+					</CardActionArea>
+				</Card>
+			</Stack>
+
+			<Dialog
+				open={isShowSearch}
+				onClose={() => {
+					setIsShowSearch(false);
+					navigate('/home');
+				}}
+				fullWidth
+			>
+				<DialogTitle>
+					<Typography variant='h6' component='div'>
+						マイ駅・停留所を追加
+					</Typography>
+				</DialogTitle>
+				<DialogContent>
+					<StationSelecter
+						onChange={(selected) => {
+							if (selected) {
+								addMyStation({ id: selected.value, role: selected.role });
+								setIsShowSearch(false);
+								navigate('/home');
+							}
+						}}
+						autoFocus
+						disabledStations={myStations.map((s) => s.name)}
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button
+						onClick={() => {
+							navigate('/home');
+							setIsShowSearch(false);
+						}}
+					>
+						閉じる
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</Box>
+	);
 }
