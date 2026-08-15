5ec74e1510072ebfbfdd81177cdbf0dbc412b948
puddinging2209 <jinjin2008jp@gmail.com>
2026-08-15 16:53:48 +0900

    0.2倍するボタンのラベルが0.25だった

--- src/features/trainMap/components/TimeControl.jsx
+++ src/features/trainMap/components/TimeControl.jsx
@@ -15,44 +15,46 @@ const MemorizedTimePicker = React.memo(({ setSecond }) => {
 			<TimePicker
 				sx={{
 					'& .MuiPickersOutlinedInput-root': {
-						padding: '8px'
+						padding: '8px',
 					},
 					'& .MuiPickersSectionList-root': {
-						display: 'none'
+						display: 'none',
 					},
 					'& .MuiInputAdornment-root': {
 						margin: 0,
-						width: '24px'
+						width: '24px',
 					},
 					'& .MuiButtonBase-root': {
-						padding: 0
-					}
+						padding: 0,
+					},
 				}}
 				ampm={false}
 				views={['hours', 'minutes', 'seconds']}
-				onChange={d => setSecond(d.hour() * 3600 + d.minute() * 60 + d.second())}
+				onChange={(d) => setSecond(d.hour() * 3600 + d.minute() * 60 + d.second())}
 			></TimePicker>
 		</LocalizationProvider>
-	)
-})
+	);
+});
 
 export function TimeControl() {
-	const [timeState, setTimeState] = useAtom(timeAtom)
-	const setSecond = useSetAtom(setSecondAtom)
-	const setSpeed = useSetAtom(setSpeedAtom)
-	const syncRealTime = useSetAtom(syncRealTimeAtom)
-	const [displayTime, setDisplayTime] = React.useState(0)
-	const [timePickerValue, setTimePickerValue] = React.useState(dayjs())
+	const [timeState, setTimeState] = useAtom(timeAtom);
+	const setSecond = useSetAtom(setSecondAtom);
+	const setSpeed = useSetAtom(setSpeedAtom);
+	const syncRealTime = useSetAtom(syncRealTimeAtom);
+	const [displayTime, setDisplayTime] = React.useState(0);
+	const [timePickerValue, setTimePickerValue] = React.useState(dayjs());
 
 	React.useEffect(() => {
-		let id
+		let id;
 		const tick = () => {
-			setDisplayTime(Math.floor((timeState.baseSimulationTime + (performance.now() - timeState.startAt) * timeState.speedRate / 1000) % (60 * 60 * 24)))
-			id = requestAnimationFrame(tick)
-		}
+			setDisplayTime(
+				Math.floor((timeState.baseSimulationTime + ((performance.now() - timeState.startAt) * timeState.speedRate) / 1000) % (60 * 60 * 24)),
+			);
+			id = requestAnimationFrame(tick);
+		};
 		id = requestAnimationFrame(tick);
-		return () => cancelAnimationFrame(id)
-	}, [timeState])
+		return () => cancelAnimationFrame(id);
+	}, [timeState]);
 
 	return (
 		<Stack spacing={1}>
@@ -63,7 +65,8 @@ export function TimeControl() {
 					</IconButton>
 				</Tooltip>
 				<Typography fontSize='1.65rem' fontFamily='monospace'>
-					{Math.floor(displayTime / 3600)}:{String(Math.floor((displayTime % 3600) / 60)).padStart(2, '0')}:{String(displayTime % 60).padStart(2, '0')}
+					{Math.floor(displayTime / 3600)}:{String(Math.floor((displayTime % 3600) / 60)).padStart(2, '0')}:
+					{String(displayTime % 60).padStart(2, '0')}
 				</Typography>
 				<MemorizedTimePicker setSecond={setSecond}></MemorizedTimePicker>
 			</Box>
@@ -72,40 +75,48 @@ export function TimeControl() {
 				valueLabelDisplay='off'
 				sx={{
 					'& .MuiSlider-thumb': {
-						transition: 'none'
+						transition: 'none',
 					},
 					'& .MuiSlider-track': {
-						transition: 'none'
+						transition: 'none',
 					},
 				}}
 				min={0}
 				max={60 * 60 * 24}
 				marks={new Array(24).fill(0).map((_, i) => ({
 					value: i * 60 * 60,
-					label: ''
+					label: '',
 				}))}
 				value={displayTime}
 				onChange={(_, value) => setSecond(value)}
 			></Slider>
 			<Stack direction='row' justifyContent='center' spacing={1}>
 				<Typography sx={{ whiteSpace: 'nowrap' }}>速度</Typography>
-				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate / 5)}>×0.25</CompactButton>
-				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate / 2)}>×0.5</CompactButton>
+				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate / 5)}>
+					×0.2
+				</CompactButton>
+				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate / 2)}>
+					×0.5
+				</CompactButton>
 				<CompactInput
 					size='small'
 					variant='filled'
 					sx={{ width: '3rem' }}
 					inputMode='numeric'
 					value={timeState.speedRate}
-					onChange={e => {
-						const rate = e.target.value
-						const number = Number.isNaN(Number(rate)) ? 1 : Number(rate)
-						setSpeed(number)
+					onChange={(e) => {
+						const rate = e.target.value;
+						const number = Number.isNaN(Number(rate)) ? 1 : Number(rate);
+						setSpeed(number);
 					}}
 				></CompactInput>
-				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate * 2)}>×2</CompactButton>
-				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate * 5)}>×5</CompactButton>
+				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate * 2)}>
+					×2
+				</CompactButton>
+				<CompactButton size='small' variant='outlined' onClick={() => setSpeed(timeState.speedRate * 5)}>
+					×5
+				</CompactButton>
 			</Stack>
 		</Stack>
-	)
+	);
 }
