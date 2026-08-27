dcd46497d9ba7f0115c3ef59a514f09a65f52db8
puddinging2209 <jinjin2008jp@gmail.com>
2026-08-27 16:24:56 +0900

    列車番号と森岡支線のバグ

--- tools/listTrainNumbers.mjs
+++ tools/listTrainNumbers.mjs
@@ -5,20 +5,27 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const TEMP_DIR = path.resolve(__dirname, 'temp');
-const OUT_DIR = path.resolve(__dirname, './temp');
+const INPUT_DIR = path.resolve(__dirname, '../public/oud');
+
+const OUT_DIR = path.resolve(__dirname, '../src/data');
 
 export default async function listTrainNumbers(line) {
+	const inputPath = path.join(INPUT_DIR, `${line}.json`);
+	const diagram = JSON.parse(await fs.readFile(inputPath, 'utf-8'));
 
-    const inputPath = path.join(TEMP_DIR, `${line}.json`);
-    const diagram = JSON.parse(await fs.readFile(inputPath, 'utf-8'));
+	const json = JSON.parse(await fs.readFile(path.join(OUT_DIR, 'numberList.json'), 'utf-8'));
 
-    const trainNumberList = diagram.railway.diagrams[0].trains.flat().map(t => t.number).filter(t => t !== '').sort();
+	const trainNumberList = diagram.railway.diagrams[0].trains
+		.flat()
+		.map((t) => t.number)
+		.filter((t) => t !== '')
+		.sort();
 
-    const outputPath = path.join(OUT_DIR, `numberList_${line}.json`);
-    await fs.writeFile(
-        outputPath,
-        JSON.stringify(trainNumberList, null, 2),
-        'utf-8'
-    );
+	const result = {
+		...json,
+		[line]: trainNumberList,
+	};
+
+	const outputPath = path.join(OUT_DIR, 'numberList.json');
+	await fs.writeFile(outputPath, JSON.stringify(result), 'utf-8');
 }
